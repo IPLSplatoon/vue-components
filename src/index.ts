ec8ef46b0879bb33884fe9981f3eed1a4fa42728
inkfarer <inkfarer@inkfarer.com>
2022-02-18 14:34:51 +0200

    Exports various helpers to dependents

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,8 @@ export { default as IplSpace } from './components/iplSpace.vue';
 export { default as IplToggle } from './components/iplToggle.vue';
 export { default as IplToggleButton } from './components/iplToggleButton.vue';
 export { default as IplUpload } from './components/iplUpload.vue';
+export * from './helpers/stringHelper';
+export * from './helpers/colorHelper';
+export * from './validation/validator';
+export * from './validation/numberValidators';
+export * from './validation/stringValidators';
