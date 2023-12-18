f19a43da47f3c2a2b45fca6da87efbb3f9815ad5
inkfarer <inkfarer@inkfarer.com>
2023-12-18 11:57:22 +0200

    Add examples for getting additional details from select options

--- docs-new/examples/SelectExample.vue
+++ docs-new/examples/SelectExample.vue
@@ -1,26 +1,30 @@
 <template>
-    <div class="vertical-layout width-capped-content">
-        <div>Selected: {{ selectedOption }}</div>
-        <!-- #region example -->
-        <ipl-select
-            v-model="selectedOption"
-            :options="[
-                { name: 'Option 1', value: 'opt1' },
-                { name: 'Option 2', value: 'opt2' },
-                { name: 'Option 3', value: 'opt3' },
-                { name: 'Option 4', value: 'opt4' }
-            ]"
-            :disabled="disabled"
-            label="Select"
-        />
-        <!-- #endregion example -->
-    </div>
-    <div class="horizontal-layout">
-        <ipl-checkbox
-            v-model="disabled"
-            label="Disabled"
-        />
-    </div>
+    <event-log>
+        <div class="vertical-layout width-capped-content">
+            <div>Selected: {{ selectedOption }}</div>
+            <!-- #region example -->
+            <ipl-select
+                v-model="selectedOption"
+                :options="[
+                    { name: 'Option 1', value: 'opt1' },
+                    { name: 'Option 2', value: 'opt2', additionalData: 'test-additional-data' },
+                    { name: 'Option 3', value: 'opt3' },
+                    { name: 'Option 4', value: 'opt4' }
+                ]"
+                :disabled="disabled"
+                label="Select"
+            />
+            <!-- #endregion example -->
+        </div>
+        <template #extra>
+            <div class="horizontal-layout">
+                <ipl-checkbox
+                    v-model="disabled"
+                    label="Disabled"
+                />
+            </div>
+        </template>
+    </event-log>
 </template>
 
 <script setup lang="ts">
--- docs-new/ipl-select.md
+++ docs-new/ipl-select.md
@@ -6,6 +6,13 @@ title: ipl-select
 
 ::: demo SelectExample
 
+### Accessing additional data in options
+
+By default, `<select>` elements only allow their values to be expressed as strings. To provide access the full data of 
+the option passed into this component, the `update:modelValue` event passes the full option object as the second 
+parameter of the event. Besides the required `name` and `value` parameters, these objects may contain any arbitrary data.
+(Try selecting "Option 2" in the above example.)
+
 ## Option Groups
 
 ::: demo SelectGroupsExample
